317ef22c4ebd84399856afd83948ed135926dd69
Alexander Alexeev <alekseev@trueconf.ru>
2023-02-22 20:56:01 +0100

    add user check controls component

--- src/views/Content/ActionControls.tsx
+++ src/views/Content/ActionControls.tsx
@@ -1,18 +1,24 @@
 import React, { FC, ReactElement } from 'react';
-import { Button, Icon, Stack } from '@chakra-ui/react';
+import { Button, Grid, GridItem, Icon } from '@chakra-ui/react';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import StopIcon from '@mui/icons-material/Stop';
 import ReplayIcon from '@mui/icons-material/Replay';
 
 const ActionControls: FC = (): ReactElement => {
   return (
-    <Stack w={'full'} direction={'row'} alignItems={'center'} justifyContent={'center'} spacing={6}>
-      <Button w={'full'} isLoading={false} colorScheme={'green'} variant={'outline'} leftIcon={<Icon as={PlayArrowIcon}/>}>Start</Button>
+    <Grid templateColumns='repeat(3, 1fr)' gap={6} w={'full'}>
+      <GridItem>
+        <Button w={'full'} isLoading={false} colorScheme={'green'} variant={'outline'} leftIcon={<Icon as={PlayArrowIcon}/>}>Start</Button>
+      </GridItem>
 
-      <Button w={'full'} isDisabled={true} colorScheme={'orange'} variant={'outline'} leftIcon={<Icon as={ReplayIcon}/>}>Replay last number</Button>
+      <GridItem>
+        <Button w={'full'} isDisabled={true} colorScheme={'orange'} variant={'outline'} leftIcon={<Icon as={ReplayIcon}/>}>Replay last number</Button>
+      </GridItem>
 
-      <Button w={'full'} isDisabled={true} colorScheme={'red'} variant={'outline'} leftIcon={<Icon as={StopIcon}/>}>Stop</Button>
-    </Stack>
+      <GridItem>
+        <Button w={'full'} isDisabled={true} colorScheme={'red'} variant={'outline'} leftIcon={<Icon as={StopIcon}/>}>Stop</Button>
+      </GridItem>
+    </Grid>
   );
 };
 
--- src/views/Content/UserCheckControls.tsx
+++ src/views/Content/UserCheckControls.tsx
@@ -1,11 +1,79 @@
 import React, { FC, ReactElement } from 'react';
-import { Stack } from '@chakra-ui/react';
+import { Button, FormControl, FormErrorMessage, FormLabel, Grid, GridItem, Icon, NumberInput, NumberInputField, Stack, Text } from '@chakra-ui/react';
+import { object, string } from 'yup';
+import { FormikHelpers, useFormik } from 'formik';
+import CheckIcon from '@mui/icons-material/Check';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+interface CheckResultDto {
+  answer: string;
+}
+
+const initialValues: CheckResultDto = {
+  answer: '',
+};
+
+const validationSchema = object().shape({
+  answer: string().required('Answer is required'),
+});
 
 const UserCheckControls: FC = (): ReactElement => {
+  const submitHandler = async (payload: CheckResultDto, formikHelpers: FormikHelpers<CheckResultDto>) => {
+    // setIsLoading(true);
+
+    try {
+      formikHelpers.setSubmitting(false);
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      // setIsLoading(false);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: submitHandler,
+    validateOnBlur: true,
+  });
+  const { touched, dirty, errors, getFieldProps } = formik;
+
   return (
-    <Stack>
-      UserCheckControls
-    </Stack>
+    <form onSubmit={formik.handleSubmit} style={{ width: '100% '}}>
+      <Stack w={'full'} direction={'column'} spacing={10}>
+        <Grid templateColumns='repeat(3, 1fr)' gap={6} w={'full'} alignItems={'end'}>
+          <GridItem>
+            <FormControl isRequired={true} isReadOnly={false} isInvalid={touched.answer && dirty && Boolean(errors.answer)}>
+              <FormLabel>Your answer</FormLabel>
+
+              <NumberInput {...getFieldProps('answer')}>
+                <NumberInputField placeholder={'Enter your answer'}/>
+              </NumberInput>
+
+              {touched.answer && dirty && Boolean(errors.answer) && <FormErrorMessage>{touched.answer && dirty && errors.answer}</FormErrorMessage>}
+            </FormControl>
+          </GridItem>
+
+          <GridItem>
+            <Button type={'submit'} isLoading={false} colorScheme={'cyan'} variant={'outline'} leftIcon={<Icon as={CheckIcon}/>} w={'full'}>
+              Check
+            </Button>
+          </GridItem>
+        </Grid>
+
+        <Grid templateColumns='repeat(3, 1fr)' gap={6} w={'full'} alignItems={'center'}>
+          <GridItem>
+            <Button isDisabled={false} colorScheme={'orange'} variant={'outline'} leftIcon={<Icon as={VisibilityIcon}/>} w={'full'}>
+              Show correct answer
+            </Button>
+          </GridItem>
+
+          <GridItem>
+            <Text>1111</Text>
+          </GridItem>
+        </Grid>
+      </Stack>
+    </form>
   );
 };
 
