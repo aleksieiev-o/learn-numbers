cd89ea5444f199572f4bf4baaff9dee7138d6b70
Alexander Alexeev <alekseev@trueconf.ru>
2023-02-25 20:26:26 +0100

    remove scss styles, remove unused packages

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,3 @@
-import axios, { AxiosInstance } from 'axios';
-import { BASE_URL } from './constants';
-
-export const API: AxiosInstance = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'Content-Type': 'application/json;charset=UTF-8',
-  },
-});
-
 export const hexToRgb = (hex: string): {r: number, g: number, b: number} => {
   const bigint = parseInt(hex.replace('#', ''), 16);
 
